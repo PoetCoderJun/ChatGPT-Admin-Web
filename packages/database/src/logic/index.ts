2c863b11062451f639505330ae15d2146edf6f93
zujun.hzj <zujun.hzj@alibaba-inc.com>
2023-05-21 14:08:24 +0800

    update

--- packages/database/src/logic/index.ts
+++ packages/database/src/logic/index.ts
@@ -1,6 +1,6 @@
 export * from "./user";
 export * from "./invitationCode";
-export * from "./registerCode";
+// export * from "./registerCode";
 export * from "./subscription";
 export * from "./accessControl";
 export * from "./order";
